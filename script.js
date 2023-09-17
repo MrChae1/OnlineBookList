e6b30db19c29c336a6fa9a784fe17ccd99d3ed5e
MrChae1 <ljnellam27@gmail.com>
2023-09-17 23:52:50 +0700

    Adding functionality to the button in validation div, add funtionality to the delete button in div element that have a class of card-container in section tag

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const myLibrary = [];
+let myLibrary = [];
 const bookName = document.getElementById('bookname');
 const authorName = document.getElementById('authorname');
 const addBtn = document.getElementById('add-btn');
 const tellSomething = document.getElementById('Book-details');
 let bookContainer = document.querySelector('.Book-Container');
+const valid = document.querySelector('.validation');
 
 
 addBtn.addEventListener('click', () =>{
@@ -11,37 +12,98 @@ addBtn.addEventListener('click', () =>{
     let Bookname = bookName.value;
     let Authorname = authorName.value;
     let Tellsomething = tellSomething.value;
-    let book = new Book(Bookname, Authorname, Tellsomething)
-    myLibrary.push(book);
-    console.log(myLibrary);
-    addBookToLibrary();
+    let indexCount = myLibrary.length;
+    validate(Bookname, Authorname,Tellsomething, indexCount );
+    tellSomething.value = '';
+    authorName.value = ''
+    bookName.value = "";
+
+    // let book = new Book(Bookname, Authorname, Tellsomething)
+    // myLibrary.push(book);
+    // addBookToLibrary();
 });
 
-function Book(bookName,authorName,tellSomething) {
+function validate(Bookname, Authorname, Tellsomething, indexCount){
+    if(Bookname === '' || Authorname === '' || Tellsomething === ''){
+        valid.style.display = 'flex';
+        bookName.disabled = true;
+        authorName.disabled = true;
+        tellSomething.disabled = true;
+        addBtn.disabled = true;
+        addBookToLibrary(myLibrary);
+    }
+    else{
+        let book = new Book(Bookname, Authorname, Tellsomething, indexCount);
+        myLibrary.push(book);
+        addBookToLibrary(myLibrary);
+    }
+
+}
+function afterRemove(){
+    bookContainer.textContent = ``;
+    let newindexCount = 0;
+    let newLibrary = [];
+    for(const newlibrary of myLibrary){
+        let newBook = newlibrary.bookName;
+        let newAuthor = newlibrary.authorName;
+        let newSypnosis = newlibrary.tellSomething;
+        let newIndex = newindexCount;
+        newindexCount++;
+        let allNewbook = new Book(newBook, newAuthor, newSypnosis, newIndex);
+        newLibrary.push(allNewbook);
+    }
+    myLibrary = newLibrary;
+    addBookToLibrary(myLibrary);
+}
+const exitBtn = document.getElementById('validation-exit').addEventListener('click', () =>{
+    valid.style.display = 'none';
+    bookName.disabled = false;
+    authorName.disabled = false;
+    tellSomething.disabled = false;
+    addBtn.disabled = false;
+});
+
+function Book(bookName,authorName,tellSomething, indexCount) {
     this.bookName = bookName,
     this.authorName = authorName,
-    this.tellSomething = tellSomething 
+    this.tellSomething = tellSomething,
+    this.indexCount = indexCount 
 }
-function addBookToLibrary() {
-    for(const library of myLibrary){
+function addBookToLibrary(allLibrary) {
+    for(const library of allLibrary){
         const CardContainer = document.createElement("div");
         CardContainer.classList.add('card-holder');
+        CardContainer.setAttribute("data-id", library.indexCount);
         let Title = document.createElement("h3");
         Title.textContent = library.bookName;
         let Author = document.createElement("p");
         Author.textContent = `Author: ${library.authorName}`;
         let Sypnosis = document.createElement("p");
-        Sypnosis.textContent = `Sypnosis; ${library.tellSomething}`;
+        Sypnosis.textContent = `Sypnosis: ${library.tellSomething}`;
         let delBtn = document.createElement("button");
         delBtn.textContent = `Delete`;
+        delBtn.addEventListener('click',() =>{
+            removeBook(library.indexCount);
+        })
         CardContainer.appendChild(Title);
         CardContainer.appendChild(Author);
         CardContainer.appendChild(Sypnosis);
         CardContainer.appendChild(delBtn);
         bookContainer.appendChild(CardContainer); 
-    }
-
-    
+    } 
 }
+function removeBook(indexRemove){
+    for(const removeBook of myLibrary){
+        if(removeBook.indexCount === indexRemove){
+            myLibrary.splice(indexRemove, 1);
+            const elementsToRemove = document.querySelectorAll(`[data-id="${indexRemove}"]`); // Replace "2" with the specific value you want to match
+            elementsToRemove.forEach(function (element) {
+            element.remove();
+            afterRemove();
+  });
+        }
+    }
+}
+
 
 
