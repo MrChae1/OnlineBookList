1a93cbb6d52b51303ba5857dfed7cd613edead80
MrChae1 <ljnellam27@gmail.com>
2023-09-30 03:18:18 +0700

    Use factories and modular to add functionality to the project

--- script.js
+++ script.js
@@ -1,120 +1,144 @@
-let myLibrary = [];
-const bookName = document.getElementById('bookname');
-const authorName = document.getElementById('authorname');
-const addBtn = document.getElementById('add-btn');
-const tellSomething = document.getElementById('Book-details');
-let bookContainer = document.querySelector('.Book-Container');
-const valid = document.querySelector('.validation');
-let showSection = document.getElementById('show-section');
-const mainContainer = document.getElementById('main-tag');
-const sectionpanel = document.getElementById('for-section');
-const sectionexiBtn = document.getElementById('exit-add');
-const resect = document.querySelector('.reSection');
-
-function clearInput(){
-    const removeValue = [tellSomething, authorName,bookName];
-    removeValue.forEach(RValue => RValue.value = '');
-    resect.style.display = 'none';
-    mainContainer.style.display = 'flex';
-}
-showSection.addEventListener('click', () =>{
-    resect.style.display = 'grid';
-    mainContainer.style.display = 'none';
-});
-addBtn.addEventListener('click', () =>{
-    bookContainer.textContent = ``;
-    let Bookname = bookName.value;
-    let Authorname = authorName.value;
-    let Tellsomething = tellSomething.value;
-    let indexCount = myLibrary.length;
-    validate(Bookname, Authorname,Tellsomething, indexCount );
+function bookLibrary(){
+    let myLibrary = [];
+    const resect = document.querySelector('.reSection');
+    const mainContainer = document.getElementById('main-tag');
+    const bookContainer = document.querySelector('.Book-Container');
+    const bookName = document.getElementById('bookname');
+    const authorName = document.getElementById('authorname');
+    const tellSomething = document.getElementById('Book-details');
+    const valid = document.querySelector('.validation');
+    const AllBtn = document.querySelector('.for-button');
+    const forbtn = AllBtn.querySelectorAll('button');
     
-});
-sectionexiBtn.addEventListener('click', () =>{
-    clearInput();
-});
-
-function validate(Bookname, Authorname, Tellsomething, indexCount){
-    if(Bookname === '' || Authorname === '' || Tellsomething === ''){
-        valid.style.display = 'flex';
-        const elementsToDisable = [bookName, authorName, tellSomething, addBtn, sectionexiBtn];
-        elementsToDisable.forEach(element => element.disabled = true);
-        addBookToLibrary(myLibrary);
+    function Book(bookName,authorName,tellSomething, indexCount) {
+        this.bookName = bookName,
+        this.authorName = authorName,
+        this.tellSomething = tellSomething,
+        this.indexCount = indexCount 
     }
-    else{
-        clearInput();
-        let book = new Book(Bookname, Authorname, Tellsomething, indexCount);
-        myLibrary.push(book);
-        addBookToLibrary(myLibrary);
+    
+    const gotoSection = () => {
+        const showSection = document.getElementById('show-section');
+        showSection.addEventListener('click', () => {
+            reDisplay();
+        });
     }
-
-}
-function afterRemove(){
-    bookContainer.textContent = ``;
-    let newindexCount = 0;
-    let newLibrary = [];
-    for(const newlibrary of myLibrary){
-        let newBook = newlibrary.bookName;
-        let newAuthor = newlibrary.authorName;
-        let newSypnosis = newlibrary.tellSomething;
-        let newIndex = newindexCount;
-        newindexCount++;
-        let allNewbook = new Book(newBook, newAuthor, newSypnosis, newIndex);
-        newLibrary.push(allNewbook);
+    const reDisplay = () => {
+        resect.style.display = resect.style.display === 'grid' ? 'none' : 'grid';
+        mainContainer.style.display = mainContainer.style.display === 'none' ? 'flex' : 'none';
+        const removeValue = [tellSomething, authorName,bookName];
+        removeValue.forEach(RValue => RValue.value = '');
     }
-    myLibrary = newLibrary;
-    addBookToLibrary(myLibrary);
-}
-const exitBtn = document.getElementById('validation-exit').addEventListener('click', () =>{
-    valid.style.display = 'none';
-    const elementsToDisable = [bookName, authorName, tellSomething, addBtn, sectionexiBtn];
-        elementsToDisable.forEach(element => element.disabled = false);
-});
-
-function Book(bookName,authorName,tellSomething, indexCount) {
-    this.bookName = bookName,
-    this.authorName = authorName,
-    this.tellSomething = tellSomething,
-    this.indexCount = indexCount 
-}
-function addBookToLibrary(allLibrary) {
-    for(const library of allLibrary){
-        const CardContainer = document.createElement("div");
-        CardContainer.classList.add('card-holder');
-        CardContainer.setAttribute("data-id", library.indexCount);
-        let Title = document.createElement("h3");
-        Title.textContent = library.bookName;
-        let mainButtonContainer = document.createElement("div");
-        mainButtonContainer.classList.add('main-button');
-        let Author = document.createElement("p");
-        Author.textContent = `Author: ${library.authorName}`;
-        let Sypnosis = document.createElement("button");
-        Sypnosis.textContent = `View Sypnosis`;
-        Sypnosis.addEventListener("click", () => showSypnosis(library.indexCount, CardContainer));
-        let delBtn = document.createElement("button");
-        delBtn.textContent = `Delete`;
-        delBtn.addEventListener('click', () => removeBook(library.indexCount) );
-        //Appending part
-        CardContainer.append(Title, Author, mainButtonContainer);
-        mainButtonContainer.append(Sypnosis, delBtn);    
-        bookContainer.appendChild(CardContainer); 
+    const Insection = () => {
+        AllBtn.addEventListener('click', (e) => {
+            if(e.target.id === 'add-btn'){
+                bookContainer.textContent = ``;
+                const title = bookName.value;
+                const author = authorName.value;
+                const say = tellSomething.value;
+                const indexCount = myLibrary.length;
+                Validate(title, author, say, indexCount);
+            }
+            else if(e.target.id === 'exit-add'){
+                reDisplay();
+            }
+        });     
     } 
+    const Validate = (title, author, say, indexCount) => {
+        if(!title || !author || !say){
+            validateBtn();
+            addtoShelf(myLibrary, bookContainer);
+        }
+        else{
+            reDisplay();
+            const book = new Book(title, author, say, indexCount)
+            myLibrary.push(book);
+            addtoShelf(myLibrary);
+        }
+    }
+    const exitBtn = document.getElementById('validation-exit').addEventListener('click', () =>{
+        validateBtn();
+    });
+
+    const validateBtn = () => {
+        valid.style.display = valid.style.display === 'flex'? 'none': 'flex';
+        const elementsToDisable = [bookName, authorName, tellSomething, forbtn[0], forbtn[1]];
+        elementsToDisable.forEach(element => element.disabled = element.disabled === true? false : true);
+    } 
+
+    function addtoShelf(allLibrary){
+        for(const library of allLibrary){
+            const CardContainer = document.createElement("div");
+            CardContainer.classList.add('card-holder');
+            CardContainer.setAttribute("data-id", library.indexCount);
+            let Title = document.createElement("h3");
+            Title.textContent = library.bookName;
+            let mainButtonContainer = document.createElement("div");
+            mainButtonContainer.classList.add('main-button');
+            let Author = document.createElement("p");
+            Author.textContent = `Author: ${library.authorName}`;
+            let Sypnosis = document.createElement("button");
+            Sypnosis.textContent = `View Sypnosis`;
+            Sypnosis.addEventListener("click", () => showSypnosis(library.indexCount, CardContainer));
+            let delBtn = document.createElement("button");
+            delBtn.textContent = `Delete`;
+            delBtn.addEventListener('click', () => removeBook(library.indexCount) );
+            //Appending part
+            CardContainer.append(Title, Author, mainButtonContainer);
+            mainButtonContainer.append(Sypnosis, delBtn);    
+            bookContainer.appendChild(CardContainer);
+        }
+    }
+    function removeBook(indexRemove){
+        myLibrary.splice(indexRemove, 1);
+        const elementsToRemove = document.querySelectorAll(`[data-id="${indexRemove}"]`);
+        elementsToRemove.forEach(element => element.remove());
+        afterRemove();
+    }
+    function showSypnosis(indexSyp, contain){
+        const forSyp = myLibrary.filter(miLibrary => miLibrary.indexCount === indexSyp).map(miLibrary => miLibrary.tellSomething);  
+        let showSyp = document.createElement("div");
+        showSyp.classList.add('forsypnosisonly')
+        let newP = document.createElement('p')
+        const newButton = document.createElement('button');
+        newButton.textContent = 'Close';
+        newButton.addEventListener('click', () => showSyp.style.display = 'none');
+        newP.textContent = `${forSyp[0]}`;
+        showSyp.append(newP, newButton);
+        contain.appendChild(showSyp);
+     }
+
+     function afterRemove(){
+        bookContainer.textContent = ``;
+        let newindexCount = 0;
+        let newLibrary = [];
+        for(const newlibrary of myLibrary){
+            let newBook = newlibrary.bookName;
+            let newAuthor = newlibrary.authorName;
+            let newSypnosis = newlibrary.tellSomething;
+            let newIndex = newindexCount;
+            newindexCount++;
+            let allNewbook = new Book(newBook, newAuthor, newSypnosis, newIndex);
+            newLibrary.push(allNewbook);
+        }
+        myLibrary = newLibrary;
+        addtoShelf(myLibrary);
+    }
+
+    return { gotoSection, Insection }
 }
-function removeBook(indexRemove){
-            myLibrary.splice(indexRemove, 1);
-            const elementsToRemove = document.querySelectorAll(`[data-id="${indexRemove}"]`);
-            elementsToRemove.forEach(element => element.remove());
-            afterRemove();
+
+function recommendBook(){
+    const Library = bookLibrary();
+
+    function addingBook(){
+        Library.gotoSection();
+        Library.Insection(); 
+    }
+
+    addingBook();
 }
-function showSypnosis(indexSyp, contain){
-   const forSyp = myLibrary.filter(miLibrary => miLibrary.indexCount === indexSyp).map(miLibrary => miLibrary.tellSomething);  
-   let showSyp = document.createElement("div");
-   showSyp.classList.add('forsypnosisonly')
-   let newP = document.createElement('p')
-   const newButton = document.createElement('button');
-   newButton.textContent = 'Close';
-   newButton.addEventListener('click', () => showSyp.style.display = 'none');
-   newP.textContent = `${forSyp[0]}`;
-   showSyp.append(newP, newButton);
-   contain.appendChild(showSyp);
-}
+
+const start = recommendBook();
+
+
